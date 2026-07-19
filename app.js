2d99d681e00f92a3beebb5c9fe626b19e5714f8e
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-19 21:21:54 +0530

    setting basic server

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+import path from "node:path";
 import express from "express";
 
+import { router } from "./routes/index.js";
+
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(import.meta.dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
