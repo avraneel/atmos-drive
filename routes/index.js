4704972b4c650d97a3df3f81596f1686e2d9e083
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-19 22:44:46 +0530

    fix ejs read views

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,11 @@ export const router = Router();
 router.get("/", (req, res) => {
   res.render("index");
 });
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
