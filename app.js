040c84ab30534d0187e9cb9506c13cb28f34e34e
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-19 23:05:53 +0530

    added prisma client and migrations

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import path from "node:path";
+import "dotenv/config";
 import express from "express";
+import { PrismaPg } from "@prisma/adapter-pg";
 import session from "express-session";
 
 import { router } from "./routes/index.js";
@@ -7,12 +9,20 @@ import { router } from "./routes/index.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+
 app.set("view engine", "ejs");
 
 const assetsPath = path.join(import.meta.dirname, "public");
 app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use("/", router);
 
