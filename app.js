f42a1ad02177ff8638fb239ebb853cd227847db4
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-19 23:29:50 +0530

    add prisma session store

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import path from "node:path";
 import "dotenv/config";
 import express from "express";
+import session from "express-session";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client.js";
-import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 
 import { router } from "./routes/index.js";
 
@@ -20,6 +21,21 @@ const assetsPath = path.join(import.meta.dirname, "public");
 app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+    secret: process.env.SECRET,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(client, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
