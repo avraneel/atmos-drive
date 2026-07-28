c0c218154d83bcb099686cae167042c0a07c46f0
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-28 22:16:47 +0530

    add disk storage

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,20 @@ import passport from "passport";
 import multer from "multer";
 
 export const router = Router();
-const upload = new multer({ dest: "uploads/" });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    cb(null, file.fieldname + "-" + uniqueSuffix + "." + extension);
+  },
+});
+
+const upload = new multer({ storage: storage });
 
 const alphaErr = `must contain alphabets only.`;
 const lengthErr = `must be between 1 and 30 characters`;
